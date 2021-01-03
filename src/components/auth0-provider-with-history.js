9b0e50568f9e9b9de1b8ad646f7d18f68de983be
Sambasiva Andaluri <andaluri@gmail.com>
2021-01-03 12:07:15 -0500

    fixes

--- src/components/auth0-provider-with-history.js
+++ src/components/auth0-provider-with-history.js
@@ -3,8 +3,8 @@ import { useHistory } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const Auth0ProviderWithHistory = ({ children }) => {
-  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+  const domain = atob(process.env.REACT_APP_AUTH0_DOMAIN);
+  const clientId = atob(process.env.REACT_APP_AUTH0_CLIENT_ID);
 
   const history = useHistory();
 
