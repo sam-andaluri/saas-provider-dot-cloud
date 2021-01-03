acd282f6f66df386d3db8e7c7beb11d1608161ee
Sambasiva Andaluri <andaluri@gmail.com>
2021-01-03 12:32:57 -0500

    tweak

--- src/components/auth0-provider-with-history.js
+++ src/components/auth0-provider-with-history.js
@@ -5,8 +5,8 @@ import { Auth0Provider } from "@auth0/auth0-react";
 const Auth0ProviderWithHistory = ({ children }) => {
   const domain = atob(process.env.REACT_APP_AUTH0_DOMAIN);
   const clientId = atob(process.env.REACT_APP_AUTH0_CLIENT_ID);
-  console.log(domain)
-  console.log(clientId)
+  console.log(domain);
+  console.log(clientId);
   const history = useHistory();
 
   const onRedirectCallback = (appState) => {
